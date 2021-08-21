47937454ec81c21f53a46d726597e6d06db3e4fe
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-21 13:38:22 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,5 +2,11 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+v= 5.7
+min= float(input("Dame los minutos: "))
+
+cm= (min*60)*(5.7)/10
+print("Los cm recorridos por el caracol son: ",cm)
+
 if __name__ == '__main__':
     main()

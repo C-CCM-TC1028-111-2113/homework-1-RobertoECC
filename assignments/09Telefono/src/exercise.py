7c3242728cce73d538d4869dd295be04382bb4d9
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-21 13:52:11 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -3,6 +3,13 @@ def main():
     #Leer los datos
     pass
 
+msj= int(input("Dame el numero de mensajes: "))
+mg= float(input("Dame le número de megas: "))
+min= float(input("Dame le número de minutos: "))
+
+CM= (msj*0.80)+(mg*0.80)+(min*0.80)
+
+print("El costo mensual es: ",CM)
 
 if __name__ == '__main__':
     main()

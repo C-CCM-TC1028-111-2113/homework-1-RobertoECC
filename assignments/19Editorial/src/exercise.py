0d0d102a56bba25f1800513ed8d452aa8e38c502
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-23 19:12:38 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+palabras= int(input("Dame el número de palabras: "))
+completas= (palabras//475)
+medias= (palabras%475)
 
+if (completas>0) and (medias>0):
+    print("El número de páginas es: ", completas+1)
+elif (completas>0) and (medias==0):
+    print("El número de páginas es: ", completas)
 
 if __name__ == '__main__':
     main()
